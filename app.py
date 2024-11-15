8332bf0b3ea87dd98bedd173775389b7d20abd7c
surovikindmitry@ <surovikindmitry@gmail.com>
2024-11-15 16:00:23 +0300

    hello

--- app.py
+++ app.py
@@ -11,9 +11,12 @@ async def on_startup(dp):
     await on_startup_notify(dp)
     await set_default_commands(dp)
 
-
+#при запуске файла app.py выполняются следующие команды
 if __name__ == '__main__':
     from aiogram import executor
+#необходимо для опроса состояния (get updates) на сервере,
     from handlers import dp
-
+#все что прошло обработку в других функциях
+#запуск get updates на сервере
     executor.start_polling(dp, on_startup=on_startup)
+#on_startup необходим для регистрации функций, фильтров и прочего. прописано выше
---
+++ readme.txt
@@ -0,0 +1,3 @@
+1. изменить env
+2. выполнить pip install requirements.txt
+3. запускается app.py
